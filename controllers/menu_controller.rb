b173e027e4397db254858349dba9a4199b6c2afb
cwilson04 <cassiewilson4@gmail.com>
2015-11-18 15:35:05 +0000

    Checkpoint 20 completed, menus

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -18,6 +18,91 @@ class MenuController
        
        selection = gets.to_i
        put "You picked #{selection}"
+       
+    case selection
+       
+        when 1
+            system "clear"
+            view_all_entries
+            main_menu
+        when 2   
+            system "clear"
+            create_entry
+            main_menu
+        when 3
+            system "clear"
+            search_entries
+            main_menu
+        when 4
+            system "clear"
+            read_csv
+            main_menu
+        when 5
+            puts "Goodbye!"
+            
+            exit(0)
+        else
+            system "clear"
+            puts "Sorry, that is not a valid input"
+            main_menu
+    end
     end
     
+    def view_all_entries
+        @address_book.each do |entry|
+            system "clear"
+            puts entry.to_s
+            
+            entry_submenu(entry)
+        end
+        
+        system "clear"
+        puts "End of entries"
+    end
+    
+    def create_entry
+        system "clear"
+        puts "New AddressBloc entry"
+        print "Name: "
+        name = gets.chomp
+        print "Phone Number: "
+        phone_number = gets.chomp
+        print "Email: "
+        email = gets.chomp
+        
+        @address_book.add_entry(name, phone_number, email)
+        
+        system "clear"
+        puts "New entry created"
+    end
+    
+    def search_entries
+    end
+    
+    def read_csv
+    end
+    
+    def entry_submenu(entry)
+        puts "n - next entry"
+        puts "d - delete entry"
+        puts "e - edit this entry"
+        puts "m - return to main menu"
+        
+        selection = gets.chomp
+        
+        case selection
+            when "n"
+            when "d"
+            when "e"
+            when "m"
+                system "clear"
+                main_menu
+            else
+                system "clear"
+                puts "#{selection} is not a valid input"
+                entries_submenu(entry)
+        end
+    end
 end
+
+
