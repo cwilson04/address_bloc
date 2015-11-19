9c9e0d796d0ffbcd743f98b710d954b418384285
cwilson04 <cassiewilson4@gmail.com>
2015-11-19 16:20:47 +0000

    Checkpoint 21 CSV

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,5 @@
 require_relative 'entry'
-
+require 'csv'
 
 class AddressBook
   attr_accessor :entries
@@ -19,11 +19,22 @@ class AddressBook
         
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
+  
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
     
+    
+  #should I keep remove_entry in code from previous assignment
   def remove_entry(name, phone_number, email)
     @entries.each do |entry|
       if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        @entries.delete(delete_entry)
+        @entries.delete(entry)
         break
       end
     end
